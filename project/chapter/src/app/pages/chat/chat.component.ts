dc955f5cc7f3df041ae23f264c8c5f7d7d7066f8
Wictor Chaves <wictor@hotmail.com.br>
2022-05-20 23:18:10 -0300

    Layout pronto

--- project/chapter/src/app/pages/chat/chat.component.ts
+++ project/chapter/src/app/pages/chat/chat.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-chat',
+  selector   : 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls  : ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
+    var messageBody           = document.querySelector('#messages');
+        messageBody.scrollTop = messageBody.scrollHeight - messageBody.clientHeight;
   }
 
 }
